5dd4f74dc85bd7fe1e0feda54a0dbb260973f816
dancsikdavid <130552807+dancsikdavid@users.noreply.github.com>
2023-11-15 11:29:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+document.getElementById("open-classroom-button").addEventListener("click", function () {
+
+    var selectedClassroom = document.getElementById("classroom-select").value;
+
+
+    if (selectedClassroom === "terem1") {
+        window.location.href = "szmt1.html";
+    
+    } else {
+    }
+    if (selectedClassroom === "terem2") {
+        window.location.href = "szmt2.html";
+    
+    } else {
+
+    }
+    if (selectedClassroom === "terem3") {
+        window.location.href = "szmt3.html";
+    
+    } else {
+
+    }
+    if (selectedClassroom === "terem4") {
+        window.location.href = "szmt4.html";
+    
+    } else {
+
+    }
+    if (selectedClassroom === "terem5") {
+        window.location.href = "szmt5.html";
+    
+    } else {
+
+    }
+    if (selectedClassroom === "terem6") {
+        window.location.href = "szmt6.html";
+    
+    } else {
+
+    }
+    if (selectedClassroom === "terem7") {
+        window.location.href = "szmt7.html";
+    
+    } else {
+    }
+
+});
---
+++ szmt.js
@@ -0,0 +1,57 @@
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var target = event.target;
+
+    if (target.className === "seat") {
+        var oldName = target.innerText;
+        var draggedSeat = document.getElementById(data);
+        var seatContent = target.innerText;
+
+        target.innerText = draggedSeat.innerText;
+        draggedSeat.innerText = seatContent;
+    }
+}
+
+function handleFileSelect(event) {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const fileContent = e.target.result;
+            const namesArray = fileContent.split('\n').filter(name => name.trim() !== '');
+            const namesContainer = document.getElementById("names");
+            namesContainer.innerHTML = '';
+            namesArray.forEach(function (name, index) {
+                const nameElement = document.createElement("div");
+                nameElement.className = "name";
+                nameElement.id = "name" + index; // Egyedi azonosítók
+                nameElement.draggable = true;
+                nameElement.innerText = name;
+                nameElement.setAttribute("ondragstart", "drag(event)");
+                namesContainer.appendChild(nameElement);
+            });
+        };
+        reader.readAsText(file);
+    }
+}
+// koko.js
+
+// Vissza a főoldalra gomb kiválasztása
+var backButton = document.getElementById('back-to-main-button');
+
+// Gombra kattintás eseménykezelő hozzáadása
+backButton.addEventListener('click', function() {
+    // Visszalépés az előző oldalra
+    window.history.back();
+});
